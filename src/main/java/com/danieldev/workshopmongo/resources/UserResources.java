692cd4e23c697a3be7e4a781deea3b8abb99a29e
Daniel Lucas <daniel.lucas1507@gmail.com>
2024-03-09 10:15:31 -0300

    feat: retornando dados do banco de dados

--- src/main/java/com/danieldev/workshopmongo/resources/UserResources.java
+++ src/main/java/com/danieldev/workshopmongo/resources/UserResources.java
@@ -1,26 +1,28 @@
 package com.danieldev.workshopmongo.resources;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.danieldev.workshopmongo.domain.User;
+import com.danieldev.workshopmongo.services.UserService;
 
 @RestController
 @RequestMapping(value = "/users")
 public class UserResources {
-	
+
+	@Autowired
+	private UserService service;
+
 	@GetMapping
-	public ResponseEntity<List<User>> findAll(){
-		User teste = new User("1", "Daniel", "Daniel@gmail.com");
-		User teste2 = new User("1", "Daniel", "Daniel@gmail.com");
-		List<User> list = new ArrayList<User>();
-		list.addAll(Arrays.asList(teste, teste2));
+	public ResponseEntity<List<User>> findAll() {
+		List<User> list = service.findAll();
+		list.addAll(Arrays.asList());
 		return ResponseEntity.ok().body(list);
 	}
 }
